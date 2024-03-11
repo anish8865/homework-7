0b5f302a35779858cf2920a13c07ae97a23f08a3
anish8865 <157658878+anish8865@users.noreply.github.com>
2024-03-11 15:40:54 -0400

    Update index.php

--- public/index.php
+++ public/index.php
@@ -6,14 +6,28 @@ require_once "../app/models/Post.php";
 require_once "../app/controllers/MainController.php";
 require_once "../app/controllers/UserController.php";
 require_once "../app/controllers/PostController.php";
+
 use app\controllers\MainController;
-use app\controllers\UserController;
 use app\controllers\PostController;
 
+$url = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 
-$url = $_SERVER["REQUEST_URI"];
 
-//todo add a switch statement router to route based on the url
-//if it is "/posts" return an array of posts via the post controller
-//if it is "/" return the homepage view from the main controller
-//if it is something else return a 404 view from the main controller
+$mainController = new MainController();
+$postController = new PostController();
+
+
+switch ($url) {
+    case '/posts':
+        
+        $postController->showPosts();
+        break;
+    case '/':
+        
+        $mainController->homepage();
+        break;
+    default:
+        
+        $mainController->notFound();
+        break;
+}
